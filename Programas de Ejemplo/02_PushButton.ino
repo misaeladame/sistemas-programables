289dd801d1dd46bb6455ad0dab294591f373ab6b
Misael Adame <misael_adame@protonmail.com>
2021-10-10 22:25:03 -0500

    Push Button

---
+++ Programas de Ejemplo/02_PushButton.ino
@@ -0,0 +1,22 @@
+int BUTTON = 2;
+int LED = 3;
+int BUTTONstate = 0;
+
+void setup()
+{
+  pinMode(BUTTON, INPUT);
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  BUTTONstate = digitalRead(BUTTON);
+  if (BUTTONstate == HIGH)
+  {
+    digitalWrite(LED, HIGH);
+  } 
+  else
+  {
+    digitalWrite(LED, LOW);
+  }
+}
