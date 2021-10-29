4aa5007f7902f0ed6cbefce02a316c2d4d0f5334
Misael Adame <misael_adame@protonmail.com>
2021-10-28 19:40:51 -0500

    Parte 1 Evaluacion

---
+++ Unidad 2/Evaluación/7_1.ino
@@ -0,0 +1,25 @@
+#include <IRremote.h>    //including infrared remote header file
+
+int RECV_PIN = 11;        // the pin where you connect the output pin of IR sensor 
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+ 
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+ 
+void loop() 
+{
+  if (irrecv.decode(&results)) 
+    {
+    int value = results.value;
+    Serial.println(" ");
+    Serial.print("Code: ");
+    Serial.println(results.value); //prints the value a a button press
+    Serial.println(" ");
+    irrecv.resume();              // Receive the next value
+    Serial.println("*****************");
+      }
+}
